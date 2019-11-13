2b7217188b8cf31439535c0755ef69360065fe07
cjlozano <carlosjlp@gmail.com>
2019-11-13 13:36:50 +0100

    configuracion theme basico vuetify

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <v-app-bar
       app
       color="primary"
-      dark
     >
       <div class="d-flex align-center">
         <v-img
@@ -38,19 +37,40 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
+    <footer class="footer">
+       <div>
+         <div class="footer__container">
+           <div class="footer__container__texto">
+             <p>Puedes enviarme un saludo escribéndome un
+               <a href="mailto:carlosjlp@gmail.com">email</a> o un
+               <a
+                 href="https://twitter.com/carlosjlp"
+                 target="__blank">tweet </a>. Además puedes cotillear mi github en
+               <a
+                 href="https://github.com/devcjlozano"
+                 target="__blank">devcjlozano</a>
+             </p>
+             <p class="footer__container__texto--info">
+                @2019 - Web app programada y diseñada por
+                <a
+                  href="https://twitter.com/carlosjlp"
+                  target="__blank">Carlos Javier </a> usando Vue + Vuetify
+              </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
@@ -58,3 +78,18 @@ export default {
   })
 }
 </script>
+<style scoped>
+ .footer {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  background-color: #fafafa;
+}
+.footer__container__texto {
+  max-width: 600px;
+  text-align: center;
+}
+.footer__container__texto--info {
+  color: #4e4e4e;
+}
+</style>
