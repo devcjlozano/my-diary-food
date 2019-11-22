4f825360875efbdaab8b851ffa691f3637eae1b6
cjlozano <carlosjlp@gmail.com>
2019-11-22 11:55:51 +0100

    Creación de vista signUp y el componente formSignUp. Maquetación inicial terminada

---
+++ src/components/FormSignUp.vue
@@ -0,0 +1,155 @@
+<template>
+  <div
+    class="form-signup">
+    <div class="form-signup--title">
+       <div>
+         <v-btn
+           @click="previousStep"
+           icon
+           color="white"
+           v-if="step === 2"
+           >
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+       </div>
+       <span class="logo"> My diary food</span>
+       <v-btn
+         :disabled="disabledButtonNext"
+         @click="nextStep"
+         dark
+         color="#110133"> Siguiente </v-btn>
+    </div>
+    <div class="form-signup--subtitle">
+       <div>
+         <span> Crea tu cuenta</span>
+       </div>
+    </div>
+    <div
+        v-if="step === 1"
+        class="form-signup--name">
+      <span> Nombre </span>
+      <v-text-field
+        color="#110133"
+        v-model="name"
+        outlined
+        placeholder="Nombre">
+      </v-text-field>
+    </div>
+    <div
+      v-if="step === 1"
+      class="form-signup--surnames">
+      <span> Apellidos </span>
+      <v-text-field
+        color="#110133"
+        v-model="surnames"
+        outlined
+        placeholder="Apellidos">
+      </v-text-field>
+    </div>
+    <div
+      v-if="step === 2"
+      class="form-signup--mail">
+      <span> Correo electrónico </span>
+      <v-text-field
+        color="#110133"
+        v-model="email"
+        outlined
+        placeholder="Correo electrónico">
+      </v-text-field>
+    </div>
+    <div
+      v-if="step === 2"
+      class="form-signup--password">
+      <span> Contraseña </span>
+      <v-text-field
+        color="#110133"
+        type="password"
+        v-model="password"
+        outlined
+        placeholder="Contraseña">
+        </v-text-field>
+     </div>
+     <div
+       v-if="step === 2"
+       class="form-signup--register">
+        <v-btn
+           class="form-signup--register--button"
+           height="56px"
+           dark
+           color="#110133"
+         >
+         Registrarse
+        </v-btn>
+     </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormSignUp',
+  data () {
+    return {
+      step: 1,
+      email: '',
+      name: '',
+      surnames: '',
+      password: ''
+    }
+  },
+  computed: {
+    disabledButtonNext () {
+      if (this.name.length === 0 || this.surnames.length === 0 || this.step === 2) {
+        return true
+      }
+      return false
+    }
+  },
+  methods: {
+    nextStep () {
+      this.step = 2
+    },
+    previousStep () {
+      this.step = 1
+    }
+  }
+}
+</script>
+<style scoped>
+.form-signup {
+  background-color: #00aeaa;
+  color: #110133;
+  padding: 10px 20px 40px 20px;
+  border-radius: 10px;
+  width: 100%;
+}
+.logo {
+  font-size: 1.1em;
+}
+.form-signup--title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+}
+.form-signup--subtitle {
+  margin-bottom: 15px;
+  font-size: 1.2em;
+}
+ .form-signup--mail,
+ .form-signup--name,
+ .form-signup--surnames,
+ .form-signup--password {
+   display: flex;
+   flex-direction: column;
+   color: #110133
+ }
+ .form-signup--register--button {
+    width: 100%;
+    display: flex;
+ }
+ div /deep/ .v-text-field--outlined fieldset {
+   border-color: #110133;
+ }
+ div /deep/ .v-input input {
+   color: red;
+ }
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="signup">
+    <FormSignUp/>
+  </div>
+</template>
+<script>
+import FormSignUp from '@/components/FormSignUp.vue'
+export default {
+  name: 'Signup',
+  components: {
+    FormSignUp
+  }
+}
+</script>
+<style scoped>
+  .signup {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 20px;
+  }
+</style>
