e25b186a9c628d5d6594bc261860ac3ded0ed719
cjlozano <carlosjlp@gmail.com>
2019-12-19 14:07:21 +0100

    rediseño tabla Componente menu Creator, días en cabeceras th

--- src/views/MenuCreator.vue
+++ src/views/MenuCreator.vue
@@ -12,24 +12,66 @@
           <template v-slot:default>
             <thead>
               <tr>
-                <th class="text-center">Dia</th>
-                <th class="text-center">DESAYUNO</th>
-                <th class="text-center">SNACK</th>
-                <th class="text-center">ALMUERZO</th>
-                <th class="text-center">MERIENDA</th>
-                <th class="text-center">CENA</th>
+                <th class="text-center"></th>
+                <th class="text-center">Lunes</th>
+                <th class="text-center">Martes</th>
+                <th class="text-center">Miércoles</th>
+                <th class="text-center">Jueves</th>
+                <th class="text-center">Viernes</th>
+                <th class="text-center">Sábado</th>
+                <th class="text-center">Domingo</th>
               </tr>
             </thead>
             <tbody class="">
               <tr
                  v-for="dayWeek in menu"
                 :key="dayWeek.dia">
-                <td style="width:2%">{{ dayWeek.day.charAt(0) }}</td>
-                <td class="td-input"><v-textarea rows="4" label="Escriba cosas" solo no-resize></v-textarea></td>
-                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
-                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
-                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
-                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
+                <td
+                  class="td-food"
+                  style="width:2%">{{ dayWeek.nameFood }}</td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    label="Escriba alimentos"
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
+                <td class="td-input">
+                  <v-textarea
+                    :rows="esSnack(dayWeek.nameFood) ? '3' : '5'"
+                    solo
+                    no-resize></v-textarea>
+                </td>
               </tr>
             </tbody>
           </template>
@@ -46,28 +88,248 @@ export default {
   data: () => ({
     menu: [
       {
-        day: 'Lunes'
+        nameFood: 'Desayuno',
+        daysFood: [
+          {
+            lunes: {
+              nameDay: 'Lunes',
+              food: 'Colacao'
+            }
+          },
+          {
+            martes: {
+              nameDay: 'Martes',
+              food: 'Colacao'
+            }
+          },
+          {
+            miercoles: {
+              nameDay: 'Miércoles',
+              food: 'Colacao'
+            }
+          },
+          {
+            jueves: {
+              nameDay: 'Jueves',
+              food: 'Colacao'
+            }
+          },
+          {
+            viernes: {
+              nameDay: 'Viernes',
+              food: 'Colacao'
+            }
+          },
+          {
+            sabado: {
+              nameDay: 'Sábado',
+              food: 'Colacao'
+            }
+          },
+          {
+            domingo: {
+              nameDay: 'Domingo',
+              food: 'Colacao'
+            }
+          }
+        ]
       },
       {
-        day: 'Martes'
+        nameFood: 'Snack',
+        daysFood: [
+          {
+            lunes: {
+              nameDay: 'Lunes',
+              food: 'Colacao'
+            }
+          },
+          {
+            martes: {
+              nameDay: 'Martes',
+              food: 'Colacao'
+            }
+          },
+          {
+            miercoles: {
+              nameDay: 'Miércoles',
+              food: 'Colacao'
+            }
+          },
+          {
+            jueves: {
+              nameDay: 'Jueves',
+              food: 'Colacao'
+            }
+          },
+          {
+            viernes: {
+              nameDay: 'Viernes',
+              food: 'Colacao'
+            }
+          },
+          {
+            sabado: {
+              nameDay: 'Sábado',
+              food: 'Colacao'
+            }
+          },
+          {
+            domingo: {
+              nameDay: 'Domingo',
+              food: 'Colacao'
+            }
+          }
+        ]
       },
       {
-        day: 'Miercoles'
+        nameFood: 'Almuerzo',
+        daysFood: [
+          {
+            lunes: {
+              nameDay: 'Lunes',
+              food: 'Colacao'
+            }
+          },
+          {
+            martes: {
+              nameDay: 'Martes',
+              food: 'Colacao'
+            }
+          },
+          {
+            miercoles: {
+              nameDay: 'Miércoles',
+              food: 'Colacao'
+            }
+          },
+          {
+            jueves: {
+              nameDay: 'Jueves',
+              food: 'Colacao'
+            }
+          },
+          {
+            viernes: {
+              nameDay: 'Viernes',
+              food: 'Colacao'
+            }
+          },
+          {
+            sabado: {
+              nameDay: 'Sábado',
+              food: 'Colacao'
+            }
+          },
+          {
+            domingo: {
+              nameDay: 'Domingo',
+              food: 'Colacao'
+            }
+          }
+        ]
       },
       {
-        day: 'Jueves'
+        nameFood: 'Merienda',
+        daysFood: [
+          {
+            lunes: {
+              nameDay: 'Lunes',
+              food: 'Colacao'
+            }
+          },
+          {
+            martes: {
+              nameDay: 'Martes',
+              food: 'Colacao'
+            }
+          },
+          {
+            miercoles: {
+              nameDay: 'Miércoles',
+              food: 'Colacao'
+            }
+          },
+          {
+            jueves: {
+              nameDay: 'Jueves',
+              food: 'Colacao'
+            }
+          },
+          {
+            viernes: {
+              nameDay: 'Viernes',
+              food: 'Colacao'
+            }
+          },
+          {
+            sabado: {
+              nameDay: 'Sábado',
+              food: 'Colacao'
+            }
+          },
+          {
+            domingo: {
+              nameDay: 'Domingo',
+              food: 'Colacao'
+            }
+          }
+        ]
       },
       {
-        day: 'Viernes'
-      },
-      {
-        day: 'Sábado'
-      },
-      {
-        day: 'Domingo'
+        nameFood: 'Cena',
+        daysFood: [
+          {
+            lunes: {
+              nameDay: 'Lunes',
+              food: 'Colacao'
+            }
+          },
+          {
+            martes: {
+              nameDay: 'Martes',
+              food: 'Colacao'
+            }
+          },
+          {
+            miercoles: {
+              nameDay: 'Miércoles',
+              food: 'Colacao'
+            }
+          },
+          {
+            jueves: {
+              nameDay: 'Jueves',
+              food: 'Colacao'
+            }
+          },
+          {
+            viernes: {
+              nameDay: 'Viernes',
+              food: 'Colacao'
+            }
+          },
+          {
+            sabado: {
+              nameDay: 'Sábado',
+              food: 'Colacao'
+            }
+          },
+          {
+            domingo: {
+              nameDay: 'Domingo',
+              food: 'Colacao'
+            }
+          }
+        ]
       }
     ]
-  })
+  }),
+  methods: {
+    esSnack (nameFood) {
+      if (nameFood === 'Snack' || nameFood === 'Merienda') { return true }
+      return false
+    }
+  }
 }
 </script>
 <style scoped>
@@ -85,6 +347,12 @@ export default {
    white-space: nowrap;
    font-size: 0.8em;
  }
+ .td-food {
+   width: 2%;
+ }
+ .td-input {
+   border-bottom: none !important;
+ }
  .td-input {
    padding: 0 5px;
  }
@@ -93,10 +361,17 @@ export default {
    min-width: 1000px;
    max-width: 1600px;
  }
+ div /deep/ .v-input__slot {
+   margin-top: 8px;
+ }
+ div /deep/ .v-textarea.v-text-field--enclosed .v-text-field__slot textarea {
+   margin-top: 0;
+ }
  div /deep/ .v-textarea .v-input__control {
    font-size: 0.8em;
  }
  div /deep/ .v-textarea .v-label {
+   top: 7px !important;
    font-size: 0.9em;
  }
  div /deep/ .v-text-field__details {
