0eea4151b374928f342a8fe743a1dc614654ceef
cjlozano <carlosjlp@gmail.com>
2019-12-19 12:52:14 +0100

    diseños iniciales tabla componente menuCreator

--- src/views/MenuCreator.vue
+++ src/views/MenuCreator.vue
@@ -1,40 +1,42 @@
 <template>
-  <v-container>
-    <v-layout
+  <div class="menu_creator">
+    <div class="menu_creator__container-table"
       text-center
       wrap
     >
-      <v-flex
-        sm12
-        mb-4><h1> Nuevo Menú semanal </h1> </v-flex>
-      <v-flex>
-      <v-simple-table dense>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-center">Dia</th>
-            <th class="text-center">DESAYUNO</th>
-            <th class="text-center">SNACK</th>
-            <th class="text-center">ALMUERZO</th>
-            <th class="text-center">MERIENDA</th>
-            <th class="text-center">CENA</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in dias" :key="item.name">
-            <td style="width:2%">{{ item.nombre }}</td>
-            <td><v-textarea rows="4" label="Escriba cosas" solo no-resize></v-textarea></td>
-            <td><v-textarea rows="4" solo no-resize></v-textarea></td>
-            <td><v-textarea rows="4" solo no-resize></v-textarea></td>
-            <td><v-textarea rows="4" solo no-resize></v-textarea></td>
-            <td><v-textarea rows="4" solo no-resize></v-textarea></td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-    </v-flex>
-    </v-layout>
-  </v-container>
+      <div class="menu_creator__container-table__title">
+        <h1> Nuevo Menú semanal </h1>
+      </div>
+      <div class="menu_creator__container-table__table">
+        <v-simple-table dense>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-center">Dia</th>
+                <th class="text-center">DESAYUNO</th>
+                <th class="text-center">SNACK</th>
+                <th class="text-center">ALMUERZO</th>
+                <th class="text-center">MERIENDA</th>
+                <th class="text-center">CENA</th>
+              </tr>
+            </thead>
+            <tbody class="">
+              <tr
+                 v-for="dayWeek in menu"
+                :key="dayWeek.dia">
+                <td style="width:2%">{{ dayWeek.day.charAt(0) }}</td>
+                <td class="td-input"><v-textarea rows="4" label="Escriba cosas" solo no-resize></v-textarea></td>
+                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
+                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
+                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
+                <td class="td-input"><v-textarea rows="4" solo no-resize></v-textarea></td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+       </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,29 +44,67 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    dias: [
+    menu: [
       {
-        nombre: 'Lunes'
+        day: 'Lunes'
       },
       {
-        nombre: 'Martes'
+        day: 'Martes'
       },
       {
-        nombre: 'Miercoles'
+        day: 'Miercoles'
       },
       {
-        nombre: 'Jueves'
+        day: 'Jueves'
       },
       {
-        nombre: 'Viernes'
+        day: 'Viernes'
       },
       {
-        nombre: 'Sábado'
+        day: 'Sábado'
       },
       {
-        nombre: 'Domingo'
+        day: 'Domingo'
       }
     ]
   })
 }
 </script>
+<style scoped>
+ .menu_creator {
+   padding: 0 20px;
+ }
+ .menu_creator__container-table__title {
+   text-align: center;
+   margin: 20px 0;
+ }
+ .menu_creator__container-table__table {
+   overflow: visible;
+   overflow-x: visible;
+   overflow-y: hidden;
+   white-space: nowrap;
+   font-size: 0.8em;
+ }
+ .td-input {
+   padding: 0 5px;
+ }
+ div /deep/ .v-data-table {
+   margin: auto;
+   min-width: 1000px;
+   max-width: 1600px;
+ }
+ div /deep/ .v-textarea .v-input__control {
+   font-size: 0.8em;
+ }
+ div /deep/ .v-textarea .v-label {
+   font-size: 0.9em;
+ }
+ div /deep/ .v-text-field__details {
+   display: none
+ }
+ @media (min-width: 700px) {
+  .menu_creator {
+   padding: 0 90px;
+ }
+}
+</style>
