4a5fd961e256141cb614cbc667b5cc7252d5bba9
cjlozano <carlosjlp@gmail.com>
2020-01-08 09:34:26 +0100

    refactor menuCreator, creación componente que pinta la tabla editable y envía al padre la información

---
+++ src/components/TableEditorMenu.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="table-editor">
+    <v-simple-table dense>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center"></th>
+            <th class="text-center">Lunes</th>
+            <th class="text-center">Martes</th>
+            <th class="text-center">Miércoles</th>
+            <th class="text-center">Jueves</th>
+            <th class="text-center">Viernes</th>
+            <th class="text-center">Sábado</th>
+            <th class="text-center">Domingo</th>
+          </tr>
+        </thead>
+        <tbody class="">
+          <tr
+            v-for="foodDistribution in menu"
+            :key="foodDistribution.nameFoodDistribution">
+            <td
+              class="td-food"
+              style="width:2%">{{ foodDistribution.nameFoodDistribution }}</td>
+            <td class="td-input">
+              <v-textarea
+                 v-model="foodDistribution.daysFood.lunes.food"
+                 :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                 solo
+                 :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                 no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.martes.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.miercoles.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.jueves.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.viernes.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.sabado.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+            <td class="td-input">
+              <v-textarea
+                v-model="foodDistribution.daysFood.domingo.food"
+                :label="foodDistribution.nameFoodDistribution.toLowerCase()"
+                :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
+                solo
+                no-resize></v-textarea>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <div class="table-editor__footer">
+      <v-btn
+        @click="saveMenu"
+        class="custom-transform-class text-none btn-own"
+        color="primary">
+          Guardar menú
+      </v-btn>
+      <span class="mtable-editor__footer__text-information">
+         *Recuerda rellenar todas las comidas de la semana
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableEditorMenu',
+  props: {
+    menu: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    saveMenu () {
+      this.$emit('save-menu')
+    },
+    esSnack (nameFoodDistribution) {
+      if (nameFoodDistribution === 'Snack' || nameFoodDistribution === 'Merienda') { return true }
+      return false
+    }
+  }
+}
+</script>
+
+<style scoped>
+ div /deep/ .v-data-table {
+   margin: auto;
+   min-width: 1000px;
+   max-width: 1600px;
+ }
+ div /deep/ .v-data-table th{
+   background-color: #00918E;
+ }
+ .theme--light.v-data-table thead tr th {
+   color: white;
+ }
+ div /deep/ .v-input__slot {
+   margin-top: 8px;
+ }
+ div /deep/ .v-textarea.v-text-field--enclosed .v-text-field__slot textarea {
+   margin-top: 0;
+ }
+ div /deep/ .v-textarea .v-input__control {
+   font-size: 0.8em;
+ }
+ div /deep/ .v-textarea .v-label {
+   top: 7px !important;
+   font-size: 0.9em;
+ }
+ div /deep/ .v-text-field__details {
+   display: none
+ }
+ .table-editor__footer {
+   display: flex;
+   justify-content: flex-start;
+   flex-wrap: wrap;
+   align-items: center;
+   max-width: 1600px;
+   margin: 10px auto;
+   padding-right: 4px;
+ }
+ .table-editor__footer__text-information {
+   margin-right: 10px;
+   font-style: italic;
+ }
+</style>
--- src/views/MenuCreator.vue
+++ src/views/MenuCreator.vue
@@ -13,108 +13,22 @@
         del día y en la parte inferior dale a guardar, <strong> !así de fácil! </strong> </p>
     </div>
     <div class="menu_creator__container-table">
-      <v-simple-table dense>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-center"></th>
-              <th class="text-center">Lunes</th>
-              <th class="text-center">Martes</th>
-              <th class="text-center">Miércoles</th>
-              <th class="text-center">Jueves</th>
-              <th class="text-center">Viernes</th>
-              <th class="text-center">Sábado</th>
-              <th class="text-center">Domingo</th>
-            </tr>
-          </thead>
-          <tbody class="">
-            <tr
-              v-for="foodDistribution in menu"
-              :key="foodDistribution.nameFoodDistribution">
-              <td
-                class="td-food"
-                style="width:2%">{{ foodDistribution.nameFoodDistribution }}</td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.lunes.food"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.martes.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.miercoles.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.jueves.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.viernes.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.sabado.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-              <td class="td-input">
-                <v-textarea
-                  v-model="foodDistribution.daysFood.domingo.food"
-                  :label="foodDistribution.nameFoodDistribution.toLowerCase()"
-                  :rows="esSnack(foodDistribution.nameFoodDistribution) ? '3' : '5'"
-                  solo
-                  no-resize></v-textarea>
-              </td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </div>
-    <div class="menu_creator__footer">
-      <span class="menu_creator__footer__text-information">
-        *Recuerda rellenar todas las comidas de la semana
-      </span>
-      <v-btn
-        @click="saveMenu"
-        class="custom-transform-class text-none btn-own"
-        color="primary">
-          Guardar menú
-      </v-btn>
+      <TableEditorMenu
+        :menu="menu"
+        @save-menu="saveMenu"/>
     </div>
   </div>
 </template>
 
 <script>
+import TableEditorMenu from '@/components/TableEditorMenu'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'MenuCreator',
-
+  components: {
+    TableEditorMenu
+  },
   data: () => ({
     menu: [
       {
@@ -290,10 +204,6 @@ export default {
     })
   },
   methods: {
-    esSnack (nameFoodDistribution) {
-      if (nameFoodDistribution === 'Snack' || nameFoodDistribution === 'Merienda') { return true }
-      return false
-    },
     saveMenu () {
       const payload = {
         menu: JSON.stringify(this.menu),
@@ -335,17 +245,6 @@ export default {
    white-space: nowrap;
    font-size: 0.8em;
  }
- .menu_creator__footer {
-   display: flex;
-   justify-content: flex-end;
-   align-items: center;
-   margin-top: 10px;
-   padding-right: 4px;
- }
- .menu_creator__footer__text-information {
-   margin-right: 10px;
-   font-style: italic;
- }
  .td-food {
    width: 2%;
  }
@@ -354,33 +253,6 @@ export default {
  }
  .td-input {
    padding: 0 5px;
- }
- div /deep/ .v-data-table {
-   margin: auto;
-   min-width: 1000px;
-   max-width: 1600px;
- }
- div /deep/ .v-data-table th{
-   background-color: #00918E;
- }
- .theme--light.v-data-table thead tr th {
-   color: white;
- }
- div /deep/ .v-input__slot {
-   margin-top: 8px;
- }
- div /deep/ .v-textarea.v-text-field--enclosed .v-text-field__slot textarea {
-   margin-top: 0;
- }
- div /deep/ .v-textarea .v-input__control {
-   font-size: 0.8em;
- }
- div /deep/ .v-textarea .v-label {
-   top: 7px !important;
-   font-size: 0.9em;
- }
- div /deep/ .v-text-field__details {
-   display: none
  }
  div /deep/ .v-icon {
    color: black;
