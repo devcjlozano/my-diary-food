4b8a84b705b6253e41fcff2f7a58fa138209dde6
cjlozano <carlosjlp@gmail.com>
2020-04-30 14:27:08 +0200

    fix name sharedMenu

---
+++ src/views/SharedMenus.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="share-menus">
+    <h1> Disponible proximamente...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShareMenus'
+}
+</script>
+<style scoped>
+  .share-menus {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
